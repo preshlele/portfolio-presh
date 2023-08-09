9812f2658f67099b7c7b78c029a9c6a6f64189d9
emmens4u <emmanuel.mensah@amalitech.org>
2023-08-09 05:12:22 +0100

    fix: images not showing

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,7 +1,7 @@
 export default [
   {
     title: "Mosaic Church",
-    imgUrl: "src/assets/mosaic.png",
+    imgUrl: "public/images/mosaic.png",
     description:
       "Designed a dynamic church website leveraging Next.js and Tailwind CSS. The project served as an opportunity to explore the latest features of Next.js 13, including file-based routing, and enhance my skills in image and background manipulation within the Next.js framework. The result is a seamless and visually appealing online platform for the church community.",
     stack: ["NextJs", "Tailwind Css", "Vite"],
@@ -10,7 +10,7 @@ export default [
   },
   {
     title: "Presh's Portfolio",
-    imgUrl: "src/assets/portfolio.png",
+    imgUrl: "public/images/portfolio.png",
     description:
       "Crafted a responsive portfolio website showcasing my skills as a frontend engineer. The project exhibits little animations using Framer Motion and draws inspiration from Brittany Chiang's work while retaining a unique touch. The website serves as a testament to my skills and creativity in frontend development",
     stack: ["React", "Tailwind Css", "Framer Motion", "Vite"],
@@ -19,7 +19,7 @@ export default [
   },
   {
     title: "Dashboard",
-    imgUrl: "src/assets/dashboard-img.png",
+    imgUrl: "public/images/dashboard-img.png",
     description:
       "Developed a minimal dashboard using Next.js, Chart.js, and Tailwind CSS to visually present sales and profit data. The project aimed to solidify my understanding of Next.js fundamentals, acquaint me with Chart.js, and master creating appealing interfaces with Tailwind CSS",
     stack: ["NextJs", "Tailwind Css", "ChartJs", "react-chartjs"],
@@ -28,7 +28,7 @@ export default [
   },
   {
     title: "Notflix",
-    imgUrl: "src/assets/notflix.PNG",
+    imgUrl: "public/images/notflix.PNG",
     description:
       "A movie website utilizing ReactJS, Tailwind CSS, and Flowbite to cater to film enthusiasts. Delivered a comprehensive platform with dynamic search and filtering features, offering personalized cinematic experiences. A go-to destination for passionate movie lovers",
     stack: ["React", "Tailwind Css", "movieApi", "Flowbite"],
